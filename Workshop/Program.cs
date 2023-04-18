37edb569c8abd38c8e938bcc81d6be1353906cbc
Nikolai <dolinnikolai@gmail.com>
2023-04-18 21:56:30 +0300

    Задание № 2, программа

---
+++ Workshop/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+string[] Array = new string[m];
+
+void array(string[] stringArray)
+{
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива");
+        stringArray[i] = Console.ReadLine()!;
+    }
+}
+
+string[] symbol(string[] stringArray)
+{
+    int n = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+            n++;
+    }
+    string[] arrayres = new string[n];
+    int j = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+        {
+            arrayres[j] = stringArray[i];
+            j++;
+        }
+    }
+    return arrayres;
+}
+void printArray(string[] stringArray)
+{
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.Write(stringArray[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+array(Array);
+printArray(symbol(Array));
